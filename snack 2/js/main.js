7e2a53a0ca43ff52654e9691c7ad3b12d3ec3a13
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-16 17:00:08 +0200

    first commit + array snack 2

--- snack 2/js/main.js
+++ snack 2/js/main.js
@@ -1,3 +1,41 @@
 'use strict';
 console.log('ci sono snack 2')
 
+//Creare un array di oggetti di squadre di calcio.
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+const squadre = [
+    {
+        name: 'milan',
+        points: 0,
+        fouls:  0
+    }, 
+    {
+        name: 'juve',
+        points: 0,
+        fouls:  0
+    }, 
+    {
+        name: 'roma',
+        points: 0,
+        fouls:  0
+    }, 
+    {
+        name: 'sassuolo',
+        points: 0,
+        fouls:  0
+    },
+    {
+        name: 'atalanta',
+        points: 0,
+        fouls:  0
+    },
+]
+console.log(squadre);
+//Generare numeri random al posto degli 0 nelle proprietà Punti fatti e falli subiti.
+ function generaCasuali(num) {
+    Math.floor(Math.random() * num);
+ }
+
+
+
+
