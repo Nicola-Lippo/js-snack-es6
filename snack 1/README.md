ff1f85ad6e35f3777db47e37fe80f801d858b755
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-16 15:08:10 +0200

    snack 1 first commit + array

---
+++ snack 1/README.md
@@ -0,0 +1,3 @@
+Snack 1
+Creare un array di oggetti con le proprietà: nome e peso
+Stampare a schermo la bici con peso minore utilizzando template literal
---
+++ snack 1/js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+console.log('ci sono')
+//Creare un array di oggetti con le proprietà: nome e peso
+const myBiciArray = [
+    {
+        nome: 'super bici',
+        peso: 8,
+    },
+    {
+        nome: 'ruote chiodate',
+        peso: 15,
+    },
+    {
+        nome: 'leggerissima',
+        peso: 4,
+    },
+    {
+        nome: 'macigno',
+        peso: 20,
+    },
+]
